e9a014e5e17411e3ac7f118b767e3b2820b74eb4
sopkqlql <sopkqlql@gmail.com>
2025-07-07 10:35:18 +0000

    몇 가지 修正 및 로그인 技能 秋稼

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function toggleLoginBox() {
+    document.querySelector(".login-box").classList.toggle("active");
+  }
+
+  function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        alert("로그인 성공!");
+        console.log("User:", userCredential.user);
+      })
+      .catch((error) => {
+        alert("로그인 실패: " + error.message);
+      });
+  }
+  const firebaseConfig = {
+    apiKey: "AIzaSyAbAKoyjv1wwF-CChOZbMfrx0u2f2G9uVQ",
+    authDomain: "datinoon-login.firebaseapp.com",
+    projectId: "datinoon-login",
+    storageBucket: "datinoon-login.firebasestorage.app",
+    messagingSenderId: "668031477218",
+    appId: "1:668031477218:web:02df73f73c44dc51d985bb",
+    measurementId: "G-RPSRPQLWLT"
+  };
