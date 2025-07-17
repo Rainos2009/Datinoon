f883f61677fe8839fe3b16957891860c5035c564
sopkqlql <sopkqlql@gmail.com>
2025-07-17 10:57:36 +0000

    수정

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,6 @@
- const supabase = supabase.createClient(
+console.log("supabase 라이브러리 상태:", typeof supabase);
+
+const supabase = supabase.createClient(
   'https://wcjzwjxdpxvwrwkbjeyi.supabase.co',
   'sb_publishable_EGSt0BKAsLxDpQuzGMcBlw_IpAyOyGi'
 );
