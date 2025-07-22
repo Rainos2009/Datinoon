14e6eb1a85d3f39e406a6f44008487336584ff2a
sopkqlql <sopkqlql@gmail.com>
2025-07-22 01:32:05 +0000

    supabase-config.js 주석 처리 및 수정 6번째

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,8 @@
-console.log("supabase 라이브러리 상태:", typeof supabase);
+/* console.log("supabase 라이브러리 상태:", typeof supabase);
 
 const supabase = supabase.createClient(
   'https://wcjzwjxdpxvwrwkbjeyi.supabase.co',
   'sb_publishable_EGSt0BKAsLxDpQuzGMcBlw_IpAyOyGi'
 );
 
-window.supabase = supabase;
+window.supabase = supabase; */
