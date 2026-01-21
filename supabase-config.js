208e437ace3a1599e6d83e4b7636e4ca76882725
Rainos2009 <sopkqlql@gmail.com>
2026-01-21 16:10:16 +0900

    새로운 시작

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,8 @@
 /* console.log("supabase 라이브러리 상태:", typeof supabase);
 
 const supabase = supabase.createClient(
-  'https://wcjzwjxdpxvwrwkbjeyi.supabase.co',
-  'sb_publishable_EGSt0BKAsLxDpQuzGMcBlw_IpAyOyGi'
+  'https://lhundplpkgafoozxmiyi.supabase.co',
+  'sb_publishable_jngaJT3f3TOfJciY8LTW9A_AStej1TQ'
 );
 
 window.supabase = supabase; */
